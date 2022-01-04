976a616077cf0102b84c0d1a9db37968dc2480de
narin111 <jungnr11@naver.com>
2022-01-05 02:05:12 +0900

    singOut

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,23 +1,32 @@
 import React, { useState } from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import Auth from '../routes/Auth';
-import Home from '../routes/Home';
+import Auth from 'routes/Auth';
+import Home from 'routes/Home';
+import Navigation from 'components/Navigation';
+import Profile from 'routes/Profile';
+import { Redirect } from 'react-router';
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
           <>
             <Route exact path="/">
               <Home />
             </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+            <Redirect from="*" to="/" />
           </>
         ) : (
           <>
             <Route exact path="/">
               <Auth />
             </Route>
+            <Redirect from="*" to="/" />
           </>
         )}
       </Switch>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,19 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { authService } from 'fbase';
+import { useHistory, useNavigate } from 'react-router-dom';
 
-const Profile = () => <span>profile</span>;
-
-export default Profile;
+// 로그아웃하면 home으로 돌아가도록
+// v6에서는 Redirect 사용 못 함
+export default () => {
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push('/');
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
